817d3ef5e612fa8608ed94d3a37f9a32e576a243
Sakshi Priya <priyasakshi8989@gmail.com>
2025-03-28 14:52:12 +0530

    mutabilityAndImmutibility.js

---
+++ mutabilityAndImmutibality.js
@@ -0,0 +1,8 @@
+let str = "Sakshi";
+str [2] = "b";
+console.log(str);  //Strings are immutable, hence don't update anything value.
+
+
+let arr = [56,89,74,3,8];
+arr [4] = 1121;
+console.log(arr);  //Arrays are mutable, hence they get updated any value.
