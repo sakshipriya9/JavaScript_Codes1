6fb7ee59b69332cd7898487f79e7f3aa538b284c
Sakshi Priya <priyasakshi8989@gmail.com>
2025-03-28 14:10:53 +0530

    arraydemo.js

---
+++ arrayDemo.js
@@ -0,0 +1,11 @@
+let x = [];
+console.log(x); //print empty array
+
+let y = [1,2,3,4,false,"abc",true,NaN,null,undefined,"ghi"];
+console.log(y); //print all indices value of variable y
+
+let z = ["abc","def","ghi"];
+console.log(z); //print all indices value of variable z
+
+const arr = [1,2,3,4,5,6,7,89,246];
+console.log(arr); //print all indices value of variable arr
