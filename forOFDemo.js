a1bd02d2bdad4f4403aac79683a22c7b8d2f6f65
Sakshi Priya <priyasakshi8989@gmail.com>
2025-03-28 14:31:00 +0530

    forOfDemo.js

---
+++ forOFDemo.js
@@ -0,0 +1,11 @@
+//print array value through for-of loop
+let arr = [10,11,14,16,120,-2,-6];
+for (var value of arr){
+    console.log(value);
+}
+
+//print string value through for-of loop
+let string = "Sakshi";
+for(const value of string){
+    console.log(value);
+}
